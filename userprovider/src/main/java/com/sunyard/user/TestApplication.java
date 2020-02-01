47dd32aab6e4c24a4d43f4da5dcd76ec71d052ab
hangtoo <12783121@qq.com>
2020-02-01 16:36:19 +0800

    feat: for umi name

--- userprovider/src/main/java/com/sunyard/user/TestApplication.java
+++ userprovider/src/main/java/com/sunyard/user/TestApplication.java
@@ -1,12 +1,8 @@
 package com.sunyard.user;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -16,13 +12,5 @@ public class TestApplication {
         SpringApplication.run(TestApplication.class, args);
     }
 
-    @Slf4j
-    @RestController
-    static class TestController {
-        @GetMapping("/hello")
-        public String hello(@RequestParam String name) {
-            log.info("invoked name = " + name);
-            return "hello " + name;
-        }
-    }
+
 }
